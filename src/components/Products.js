fa6f4917e55df37ac84a5d4b12e41a4f6fa6282a
Matheus <matriblima97@gmail.com>
2023-10-27 00:54:06 +0200

    1 version

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,46 @@
-const Products = () =>{
-    return <div>
-        Mis productos
-    </div>
+import React, { useState, useEffect } from "react";
+import "./Products.css"
 
+
+const url = "http://localhost:9999/products"
+
+function Products(){
+    const [productData, setProductData] = useState({products: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const response = await fetch(url);
+                if (!response.ok) {
+
+                    throw new Error("We couldnt load JSON file.");
+                }
+                const data = await response.json();
+                setProductData({products:data});
+            } catch (error) {
+                console.error("Failed to charge the JSON file.", error);
+            }
+        };
+        fetchData();
+    },[]);
+
+    return (
+        <div>
+            <div key={"products"}>
+                <h2>Top Ventas</h2>
+                <div class="container">
+                    {productData.products.map((product, index) =>(
+                        <div class="card" key={index}>
+                            <img class="imageProduct" src={product.imagen}/>
+                            <p id="precio">{product.precio}€</p>
+                            <p id="description">{product.nombre}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
 }
+    
 
-export default  Products;
+export default Products;
