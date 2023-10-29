594bae35df8088a3b2d742dd3032485b29e4b4ea
Matheus Ribeiro <mat.rib.lima@gmail.com>
2023-10-29 12:00:33 +0100

    v3

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,22 @@ import Logo from "./Logo";
 function Header() {
     return (
         <header>
+            <div>
             <Logo/>
-            <nav>
-                <a></a>
-                <a></a>
-                <a></a>
-                <a></a>
-                <a></a>
-                <a></a>
-                <a></a>
+            <input className="inputHeader" placeholder="Busca producto, deporte..."></input>
+            </div>
+            <nav className="navigatorBar">
+                <a>Deportes</a>
+                <a>Hombre</a>
+                <a>Mujer</a>
+                <a>Infantil</a>
+                <a>Equipamiento</a>
+                <a>Nutrición</a>
+                <a>Descuentos</a>
             </nav>
         </header>
     )
 }
-
 
 
 export default Header;
